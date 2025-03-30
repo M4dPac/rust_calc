7606c0ef75cf08d08adce156b7dff1cc2587f0d6
M4dPac <pacc8891@gmail.com>
2025-03-30 14:43:33 +0300

    feat(error): реализовать форматирование и обработку ошибок
    - Реализован Display для удобного вывода ошибок
    - Добавлена поддержка трейта Error
    - Улучшены варианты ошибок (String вместо char)
    - Добавлены подробные сообщения для каждой ошибки
    - Добавлена документация для enum CalcError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,28 @@
+use std::error::Error;
+use std::fmt;
+
+// Определяем перечисление для ошибок калькулятора
+#[derive(Debug)]
 pub enum CalcError {
-    InvalidToken(char),
+    InvalidToken(String),
     UnmatchedParens,
     DivideByZero,
-    InvalidExpression,
+    InvalidExpression(String),
 }
+
+// Реализуем Display для CalcError для удобного вывода ошибок
+impl fmt::Display for CalcError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            CalcError::InvalidToken(token) => format!("Неверное число: {token}"),
+            CalcError::UnmatchedParens => "Не совпадают скобки.".to_owned(),
+            CalcError::DivideByZero => "Деление на 0.".to_owned(),
+            CalcError::InvalidExpression(expr) => format!("Некорректное выражение: {expr}"),
+        };
+
+        write!(f, "Error: {message}")
+    }
+}
+
+// Реализуем Error для CalcError для использования в контексте ошибок
+impl Error for CalcError {}
