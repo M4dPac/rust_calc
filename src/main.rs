ee15baab92bf11591bcef4b2081cf7a1101d76cb
M4dPac <pacc8891@gmail.com>
2025-03-30 11:27:55 +0300

    feat(repl): реализовать базовый ввод строки
    - Добавлена функция read_input() для безопасного чтения строк
    - Создана основа REPL-цикла в run_repl()
    - Основной цикл пока не обрабатывает ввод

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    run_repl();
+}
+
+fn read_input() -> String {
+    loop {
+        let mut s = String::new();
+        if io::stdin().read_line(&mut s).is_err() {
+            eprintln!("Ошибка ввода.");
+            continue;
+        }
+
+        return s.trim().to_owned();
+    }
+}
+
+fn run_repl() {
+    loop {
+        let input = read_input();
+    }
 }
