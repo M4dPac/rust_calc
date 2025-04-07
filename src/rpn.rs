db200df5c8c8796273fad5d7afd36a2bab6bfe20
M4dPac <pacc8891@gmail.com>
2025-04-07 12:48:57 +0300

    feat(rpn): реализовать алгоритм Shunting-yard
    - Полная реализация конвертации в обратную польскую запись
    - Обработка чисел (непосредственный вывод)
    - Обработка операторов с учетом приоритетов:
      * Выталкивание операторов с высшим приоритетом
      * Добавление текущего оператора в стек
    - Перенос оставшихся операторов в выходную очередь
    
    Особенности:
    - Используется система приоритетов из Token::precedence()
    - Пока не реализована обработка скобок

--- src/rpn.rs
+++ src/rpn.rs
@@ -2,17 +2,29 @@ use crate::{error::CalcError, parser::Token};
 
 /// Алгоритм сортировочной станции (Shunting-yard)
 pub fn to_rpn(tokens: Vec<Token>) -> Result<Vec<Token>, CalcError> {
-    let mut output = Vec::new();
-    let mut operators = Vec::new();
+    let mut output: Vec<Token> = Vec::new();
+    let mut operators: Vec<Token> = Vec::new();
 
     for token in tokens {
         match token {
             Token::Number(_) => output.push(token),
             _ => {
-                todo!()
+                while let Some(top) = operators.last() {
+                    if top.precedence() >= token.precedence() {
+                        output.push(match operators.pop() {
+                            Some(op) => op,
+                            None => todo!(),
+                        });
+                    } else {
+                        break;
+                    }
                 }
             }
         }
+    }
+
+    while let Some(op) = operators.pop() {
+        output.push(op);
     }
 
     Ok(output)
